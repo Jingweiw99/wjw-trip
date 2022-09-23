33b78185dc5db766171674c16bccee0290344880
Wristwang <2117678336@qq.com>
2022-09-23 23:43:03 +0800

    基本路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     app vue
+    <router-view></router-view>
   </div>
 
   
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+
 
 import "normalize.css"
-import "./assets/css/index.css"
+// import "./assets/css/index.css"
 
 
+createApp(App).use(router).mount('#app')
 
-const app = createApp(App)
-
-
-app.mount('#app')
