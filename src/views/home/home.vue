d916697b291cba025e3105e51616899aad4a0c23
Wristwang <2117678336@qq.com>
2022-09-26 10:48:08 +0800

    安装配置vant3

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="home">
-      <h2>home</h2>
-    </div>
+  <div class="home">
+    <h2>home</h2>
+ 
+  </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
+const ratevValue = ref(0)
+const reteIncrement = function () {
+  ratevValue.value++
+}
 </script>
 
 <style lang="less" scoped>
