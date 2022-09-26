8223327bc8a931d96c12ef604e2e6ed570ecefc0
Wristwang <2117678336@qq.com>
2022-09-26 19:33:30 +0800

    city页面的搜索

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,18 +1,32 @@
 <template>
-    <div class="city top-page">
-      <h2>city</h2>
-      <template v-for="item in 1000">
-        <div>行了{{item}}</div>
-      </template>
-    </div>
+  <div class="city top-page">
+      <van-search 
+        v-model="searchValue" 
+        placeholder="请输入搜索关键词" 
+        shape="round"
+        show-action
+        @search="onSearch"
+        @cancel="onCancel"
+       />
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const searchValue = ref('');
+const onSearch = (val) => {
+  console.log("search",val)
+};
+const onCancel = () => {
+  router.back()
+};
+
 
 </script>
 
 <style lang="less" scoped>
-.city {
-  
-}
+.city {}
 </style>
