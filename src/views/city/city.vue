5cfc92bfdba3372257f3d500fb146e221c47667f
Wristwang <2117678336@qq.com>
2022-09-26 20:01:33 +0800

    city的Tabs

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="city top-page">
-      <van-search 
-        v-model="searchValue" 
-        placeholder="请输入搜索关键词" 
-        shape="round"
-        show-action
-        @search="onSearch"
-        @cancel="onCancel"
-       />
+    <van-search 
+      v-model="searchValue" 
+      placeholder="请输入搜索关键词" 
+      shape="round"
+      show-action
+      @search="onSearch"
+      @cancel="onCancel"
+      />
+
+    <van-tabs v-model:active="active" color="var(--primary-color)" :line-height="3">
+      <van-tab title="国内‧港澳台">内容 1</van-tab>
+      <van-tab title="海外">内容 2</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -16,7 +21,11 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+
 const searchValue = ref('');
+const active = ref(0);
+
+
 const onSearch = (val) => {
   console.log("search",val)
 };
