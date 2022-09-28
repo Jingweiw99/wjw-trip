29a530c1ba299efa34c37d81074d1fc75ae4b44a
wjw <2117678336@qq.com>
2022-09-28 13:44:28 +0800

    home-search热门推荐、数据

---
+++ src/stores/modules/home.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia"
+import { getHomeHotSuggests } from "@/services"
+const useHomeStore = defineStore("home", {
+  state: () => ({
+    hotSuggests: [],
+  }),
+  actions: {
+    async fetchHotSuggestData() {
+      const res = await getHomeHotSuggests()
+      this.hotSuggests = res.data
+    }
+  }
+})
+
+export default useHomeStore
--- src/views/home/cpns/home-search-box.vue
+++ src/views/home/cpns/home-search-box.vue
@@ -39,17 +39,30 @@
     </div>
     <!-- 关键字 -->
     <div class="section keyword bottom-gray-line">关键字/位置/民宿名</div>
+    <!-- 热门建议 -->
+    <div class="section hot-suggests">
+      <template v-for="(item, index) in hotSuggests" :key="index">
+        <div 
+          class="item"
+          :style="{ color: item.tagText.color, background: item.tagText.background.color }"
+        >
+          {{ item.tagText.text }}
+        </div>
+      </template>
+    </div>
   </div>
 </template> 
   
 <script setup>
 import useCityStore from '@/stores/modules/city';
+import useHomeStore from '@/stores/modules/home'
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
 import { ref } from 'vue'
 import { formatMonthDay, getDiffDays } from '@/utils/format_date'
 
 const router = useRouter()
+
 
 const positionClick = () => {
   navigator.geolocation.getCurrentPosition(res => {
@@ -85,6 +98,10 @@ const onConfirm = (value) => {
   stayCount.value = getDiffDays(selectStartDate, selectEndDate)
   show.value = false
 }
+
+// 热门建议
+const homeStore = useHomeStore()
+const { hotSuggests } =storeToRefs(homeStore)
 </script>
 
 <style lang="less" scoped>
@@ -178,4 +195,18 @@ const onConfirm = (value) => {
     border-right: 1px solid  var(--line-color);
   }
 }
+
+.hot-suggests {
+  margin: 10px 0;
+  height: auto;
+
+  .item {
+    padding: 4px 8px;
+    margin: 4px;
+    border-radius: 14px;
+    font-size: 12px;
+    line-height: 1;
+  }
+}
+
 </style>
