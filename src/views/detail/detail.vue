d9ee5badccbd22be89a8898953b66a55abf264bf
wjw <2117678336@qq.com>
2022-09-30 18:14:33 +0800

    detail-swipe展示

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="detail top-page">
     <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="backToHome" />
-    <h2>houseId: {{ $route.params.id}}</h2>
-    {{detailInfos}}
+    <div class="main" v-if="mainPart">
+      <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
+      <h2>houseId: {{ $route.params.id}}</h2>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,8 @@
 import { useRoute, useRouter } from 'vue-router';
 import useDetailStore from "@/stores/modules/detail"
 import { storeToRefs } from 'pinia';
+import { computed } from '@vue/reactivity';
+import DetailSwipe from './cpns/detail-swipe.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -20,10 +24,12 @@ const backToHome = () => {
 // 发送网络请求
 const houseId = route.params.id
 const detailStore = useDetailStore()
-detailStore.fetchDetailInfos(houseId)
-
 const { detailInfos } = storeToRefs(detailStore)
+const mainPart = computed(() =>
+  (detailInfos.value.mainPart)
+)
 
+detailStore.fetchDetailInfos(houseId)
 </script>
 
 <style lang="less" scoped>
