56a7b749a158983f7ab05ef827b20efc3ed796c9
wjw <2117678336@qq.com>
2022-09-30 15:48:13 +0800

    detail数据分层和获取

---
+++ src/views/detail/detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="detail top-page">
+    <van-nav-bar title="房屋详情" left-text="旅途" left-arrow @click-left="backToHome" />
+    <h2>houseId: {{ $route.params.id}}</h2>
+    {{detailInfos}}
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import useDetailStore from "@/stores/modules/detail"
+import { storeToRefs } from 'pinia';
+
+const route = useRoute()
+const router = useRouter()
+// 监听点击按钮的返回
+const backToHome = () => {
+  router.back()
+}
+// 发送网络请求
+const houseId = route.params.id
+const detailStore = useDetailStore()
+detailStore.fetchDetailInfos(houseId)
+
+const { detailInfos } = storeToRefs(detailStore)
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/home/cpns/home-content.vue
+++ src/views/home/cpns/home-content.vue
@@ -3,8 +3,16 @@
     <h2 class="title">热门精选</h2>
     <div class="list">
       <template v-for="item in houselist" :key="item.data.houseId">
-        <house-item-v9 v-if="item.discoveryContentType === 9" :item-data="item.data"></house-item-v9>
-        <house-item-v3 v-else :item-data="item.data" />
+        <house-item-v9
+          v-if="item.discoveryContentType === 9"
+          :item-data="item.data"
+          @click="itemClick(item.data)" 
+        />
+        <house-item-v3 
+          v-else 
+          :item-data="item.data"
+          @click=itemClick(item.data) 
+        />
       </template>
     </div>
   </div>
@@ -15,9 +23,16 @@ import useHomeStore from '@/stores/modules/home';
 import { storeToRefs } from 'pinia';
 import houseItemV9 from '@/components/house-item-v9/house-item-v9.vue';
 import houseItemV3 from '@/components/house-item-v3/house-item-v3.vue';
+import { useRouter } from 'vue-router';
 
 const homeStore = useHomeStore()
 const { houselist } = storeToRefs(homeStore)
+
+const router = useRouter()
+const itemClick = (item) => {
+  // console.log(item)
+  router.push("/detail/" + item.houseId)
+}
 </script>
 
 <style lang="less" scoped>
